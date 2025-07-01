7a969bc2f19965f28068aae479de231a3f80fcf3
BarnabuTech <barnabutech@gmail.com>
2025-07-01 17:07:27 +0300

    Initial commit

--- pages/views.py
+++ pages/views.py
@@ -5,10 +5,13 @@
 from django.shortcuts import render
 from django.conf import settings
 
-from products.models import Product
+from products.models import Product, Wishlist
 
 def home(request):
-    products = Product.objects.filter(available=True)
+    products = Product.objects.filter(available=True).order_by('-created')[:20]
+    wishlisted_ids = set()
+    if request.user.is_authenticated:
+        wishlisted_ids = set(Wishlist.objects.filter(user=request.user).values_list('product_id', flat=True))
     
     for product in products:
         product.averageReview = product.average_review()
@@ -17,6 +20,7 @@ def home(request):
     
     context = {
         'products': products,
+        'wishlisted_ids': wishlisted_ids,
     }
     return render(request, 'pages/home.html', context)
 
