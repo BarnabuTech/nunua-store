5372c63044ed70fbd4ef8d1be2a6fda3f1e7b843
BarnabuTech <barnabutech@gmail.com>
2025-10-21 17:26:38 +0300

    fix: the wsgi for vercel deployment build error

--- nunua/wsgi.py
+++ nunua/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nunua.settings')
 
 application = get_wsgi_application()
+
+# Required for Vercel — alias 'application' as 'app'
+app = application
